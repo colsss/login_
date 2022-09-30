5eb3870094c29a75c4ae199541bae7684721f529
cols0 <114733983+cols0@users.noreply.github.com>
2022-09-30 17:37:17 +0800

    Update welcome.php
    remove letter s

--- welcome.php
+++ welcome.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcomes</title>
+    <title>Welcome</title>
 </head>
 <body>
     <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
