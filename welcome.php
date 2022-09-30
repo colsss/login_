c9293dad782978882711b94f5cc83a568ee0a4e6
cols0 <114733983+cols0@users.noreply.github.com>
2022-09-30 17:34:21 +0800

    Update welcome.php
    changes

--- welcome.php
+++ welcome.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Welcomes</title>
 </head>
 <body>
     <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
